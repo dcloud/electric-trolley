009ca3bb2902a8745f1ada2d0e4055ecac465d51
Daniel Cloud <daniel@danielcloud.org>
2015-11-09 16:49:42 -0500

    Use DoubleMap api for presenting routes list

--- src/app/routes-list.component.ts
+++ src/app/routes-list.component.ts
@@ -1,14 +1,15 @@
 import {Component, CORE_DIRECTIVES} from 'angular2/angular2';
 import {Route, RouteType} from './route';
-import {RoutesService} from './routes.service';
+import {DoubleMapApiService} from './doublemapapi.service';
 
 @Component({
     selector: 'routes-list-component',
-    providers: [RoutesService],
+    providers: [DoubleMapApiService],
     template: `
 <ul>
     <li *ng-for="#r of routes">
         <h3 class="short-name" [style.color]="'#'+r.color">Route {{ r.shortName }}: #{{r.id }}</h3>
+        <p>{{ r.longName }}</p>
     </li>
 </ul>
 `,
@@ -26,9 +27,9 @@ import {RoutesService} from './routes.service';
 export class RoutesListComponent {
     public routes: Route[];
 
-    constructor(service: RoutesService) {
-        service.fetchItems().then(() => {
-            this.routes = service.items.sort((a, b) => {
+    constructor(service: DoubleMapApiService) {
+        service.routes().then((results: Route[]) => {
+            this.routes = results.sort((a, b) => {
                 if (a.shortName < b.shortName)
                     return -1;
                 if (a.shortName > b.shortName)
