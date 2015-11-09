4729c5f0af504c4470732f4d6858f5d36f63a29e
Daniel Cloud <daniel@danielcloud.org>
2015-11-09 16:31:48 -0500

    Fix reload on js build from tsc

--- dev.js
+++ dev.js
@@ -1,11 +1,11 @@
 // require the module as normal
 var bs = require("browser-sync").create();
 
+bs.watch("*.html").on("change", bs.reload);
+bs.watch("dist/app/*.js").on("change", bs.reload);
+
 // .init starts the server
 bs.init({
-    proxy: "localhost:5000"
+    proxy: "localhost:5000",
+    open: false
 });
-
-// Now call methods on bs instead of the
-// main browserSync module export
-bs.reload("*.html", "dist/*.js");
